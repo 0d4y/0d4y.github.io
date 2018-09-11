7c39714f419c4b3e5e05f1d3e99c6e93a38f8e25
0d4y <43162249+0d4y@users.noreply.github.com>
2018-09-11 05:24:38 +0000

    Delete yesscript.php

--- yesscript.php
+++
@@ -1,9 +0,0 @@
-<html>
- <?php
-  header('Content-Type: text/html;/json');
- ?>
- <body>
-  <h1>NoScript vulnerable checker</h1>
-  <script>alert('If you are using NoScript, your browser is vulnerable to YesScript!');</script>
- </body>
-</html>
